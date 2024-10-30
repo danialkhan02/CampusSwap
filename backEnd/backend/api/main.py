006458acf979d089d972aa513308fc26a7c8d185
sulynajimsj <sulynajim2002@gmail.com>
2024-10-29 20:05:02 -0400

    register seller profiles router in main API router

--- backEnd/backend/api/main.py
+++ backEnd/backend/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Response, HTTPException, status
 from starlette.responses import JSONResponse
 
-from backend.api.routes import users, products
+from backend.api.routes import users, products, seller_profiles
 from fastapi.requests import Request
 from backend.api_responses import ApiResponse, ErrMessage
 
@@ -13,3 +13,4 @@ api_router = APIRouter(
 # Add all routes
 api_router.include_router(products, prefix="/products", tags=["products"])
 api_router.include_router(users, prefix="/users", tags=["users"])
+api_router.include_router(seller_profiles, prefix="/seller_profiles", tags=["seller_profiles"])
