ddac1ca7b2281c41de925014ce17bd44a3647788
sulynajimsj <sulynajim2002@gmail.com>
2024-10-31 14:29:23 -0400

    egister notifications router in routes module

--- backEnd/backend/api/routes/__init__.py
+++ backEnd/backend/api/routes/__init__.py
@@ -1,5 +1,6 @@
 from backend.api.routes.users import router as users
 from backend.api.routes.products import router as products
 from backend.api.routes.seller_profiles import router as seller_profiles
+from backend.api.routes.notifications import router as notifications
 
-__all__ = ["users", "products", "seller_profiles"]
+__all__ = ["users", "products", "seller_profiles", "notifications"]
