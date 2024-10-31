d0199a38315dbde56598f7e1caba080a38f030d3
sulynajimsj <sulynajim2002@gmail.com>
2024-10-31 14:28:29 -0400

    add notifications router to API endpoints

--- backEnd/backend/api/main.py
+++ backEnd/backend/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Response, HTTPException, status
 from starlette.responses import JSONResponse
 
-from backend.api.routes import users, products, seller_profiles
+from backend.api.routes import users, products, seller_profiles, notifications
 from fastapi.requests import Request
 from backend.api_responses import ApiResponse, ErrMessage
 
@@ -14,3 +14,4 @@ api_router = APIRouter(
 api_router.include_router(products, prefix="/products", tags=["products"])
 api_router.include_router(users, prefix="/users", tags=["users"])
 api_router.include_router(seller_profiles, prefix="/seller_profiles", tags=["seller_profiles"])
+api_router.include_router(notifications, prefix="/notifications", tags=["notifications"])
