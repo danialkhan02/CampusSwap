b50b39715339170d8bbe92c67bb8bbf0611dc99d
KhanDanial <danial.akh02@gmail.com>
2024-11-12 01:58:12 -0500

    Reconfigured api endpoints

--- backEnd/backend/api/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from backend.api.routes.users import router as users
-from backend.api.routes.products import router as products
-from backend.api.routes.seller_profiles import router as seller_profiles
-from backend.api.routes.seller_feedbacks import router as seller_feedbacks
-from backend.api.routes.chat import router as chat
-
-__all__ = ["users", "products", "seller_profiles", "seller_feedbacks", "chat"]
